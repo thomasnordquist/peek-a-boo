b1238158fca5cbb5e14da57cf4c2a6659c937e7a
Thomas Nordquist <thomas.nordquist@viison.com>
2015-05-18 15:46:30 +0200

    Add config docu

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-module.exports = {
+/*
+ * All time parameters are expected in seconds
+ */
+
+ module.exports = {
 	offlineAfter: 60*5, /* 5 minutes */
 	nmapBinary: '/usr/local/bin/nmap',
 	discoverInterval: 2
