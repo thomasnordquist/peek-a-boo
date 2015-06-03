2fb974f5d3d2f6be05025c7941a0c3fa8076acdb
Thomas Nordquist <thomas.nordquist@viison.com>
2015-06-03 15:44:48 +0200

    Fix offline after configuration

--- config.js
+++ config.js
@@ -4,9 +4,9 @@
 
  module.exports = {
 	 applicationPort: '8000', /* the application will be served here (also possible to bind to host: 127.0.0.1:8080)*/
-	 offlineAfter: 60*50, /* 5 minutes */
 	 nmapBinary: '/usr/bin/nmap',
 	 bindToHost: null, /* enter an ip to bind the application port to a specific ip */
+	 offlineAfter: 60*1, /* 1 minute */
 	 nmapRange: '192.168.178.1-255',
 	 discoverInterval: 2,
 	 discoveryUrl: 'http://localhost:8080', /* url where the discover updates come from */
