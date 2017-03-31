75f64e91d34998915d434a3b1a06e3907cfd025f
Thomas Nordquist <thomas.nordquist@viison.com>
2017-03-31 12:37:29 +0200

    Refactor person model

--- Models/Person.js
+++ Models/Person.js
@@ -1,6 +1,24 @@
-module.exports = function Person() {
-  this.email = null
-  this.name = null
-  this.gravatar = null
-  this.devices = {}
+const moment = require('moment')
+
+module.exports = class Person {
+  constructor({ email, name, gravatar, devices }) {
+    this.email = email
+    this.name = name
+    this.gravatar = gravatar || ''
+    this.devices = devices || []
+  }
+
+  lastSeen() {
+    const lastSeenDevice = this.devices.sort(
+      (a, b) => {
+        const dateA = a.lastSeen ? moment(a.lastSeen) : 0
+        const dateB = b.lastSeen ? moment(b.lastSeen) : 0
+        return dateA < dateB
+      })[0]
+
+    if (lastSeenDevice) {
+      return lastSeenDevice.lastSeen
+    }
+    return null
+  }
 }
