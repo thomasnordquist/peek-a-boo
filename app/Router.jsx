4a73a25d1ed1d886ef130f52c3ef2ea5d34ee4fa
Thomas Nordquist <thomas.nordquist@viison.com>
2017-03-31 12:37:29 +0200

    Render with ReactDOM

--- app/Router.jsx
+++ app/Router.jsx
@@ -1,11 +1,13 @@
 const React = require('react')
+const ReactDOM = require('react-dom')
 const Router = require('react-router')
-const Route = Router.Route
-const DefaultRoute = Router.DefaultRoute
 
 const App = require('./App')
 const Devices = require('./Devices')
 const Persons = require('./Persons')
+
+const Route = Router.Route
+const DefaultRoute = Router.DefaultRoute
 
 // polyfill
 if (!Object.assign) { Object.assign = React.__spread }
@@ -21,5 +23,5 @@ const routes = (
 // Or, if you'd like to use the HTML5 history API for cleaner URLs:
 
 Router.run(routes, Router.HistoryLocation, (Handler) => {
-  React.render(<Handler />, document.getElementById('content'))
+  ReactDOM.render(<Handler />, document.getElementById('content'))
 })
