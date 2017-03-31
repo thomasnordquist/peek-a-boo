b970bd801539a018ef0c6911b19a9269f6f6e718
Thomas Nordquist <thomas.nordquist@viison.com>
2017-03-31 12:37:29 +0200

    Refactor PersonForm

--- app/PersonForm.jsx
+++ app/PersonForm.jsx
@@ -1,30 +1,58 @@
 const React = require('react')
 
-const PersonForm = React.createClass({
+class PersonForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { email: '', name: '' }
+  }
+
   addPerson() {
-    const email = React.findDOMNode(this.refs.email).value
-    const name = React.findDOMNode(this.refs.name).value
-    if (email == '' || name == '') {
+    if (this.state.email === '' || this.state.name === '') {
       alert('Enter valid values')
     } else {
-      this.props.onAddPerson(email, name)
+      this.props.onAddPerson(this.state.email, this.state.name)
     }
-  },
+  }
+
   render() {
     return (
       <div>
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
-          <input ref="email" type="email" className="form-control" id="exampleInputEmail1" placeholder="Gravatar" />
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            placeholder="Gravatar"
+            onChange={value => this.setState({ name: value })}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Name</label>
-          <input ref="name" type="text" className="form-control" id="name" placeholder="Name" />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Name"
+            onChange={value => this.setState({ name: value })}
+          />
         </div>
-        <button onClick={this.props.onAbort} className="btn btn-danger btn-sm">Abort</button>&nbsp;
-        <button onClick={this.addPerson} type="submit" className="btn btn-primary btn-sm">Add</button>
+        <button
+          onClick={this.props.onAbort}
+          className="btn btn-danger btn-sm"
+        >Abort</button>&nbsp;
+        <button
+          onClick={this.addPerson}
+          type="submit"
+          className="btn btn-primary btn-sm"
+        >Add</button>
       </div>
     )
-  },
-})
+  }
+}
+
+PersonForm.propTypes = {
+  onAddPerson: React.PropTypes.func.isRequired,
+}
+
 module.exports = PersonForm
