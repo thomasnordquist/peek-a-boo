2647067392d9417ac4c3b8297df0050cb6403345
Thomas Nordquist <thomas.nordquist@viison.com>
2017-03-31 12:37:29 +0200

    Refactor TableRow

--- app/PersonTable/TableRow.jsx
+++ app/PersonTable/TableRow.jsx
@@ -1,25 +1,37 @@
 const React = require('react')
+const Person = require('../../Models/Person')
 const config = require('../../config')
 const moment = require('moment')
 
-const PersonLine = React.createClass({
-  isOnline(person) {
+class PersonLine extends React.Component {
+  static isOnline(person) {
     return ((moment().format('x') - person.lastSeen()) / 1000) > config.offlineAfter
-  },
+  }
+
+  static isOffline(person) {
+    return (!person.lastSeen() || PersonLine.isOnline(person))
+  }
+
   render() {
     const person = this.props.person
-    function isOffline(person) {
-      return (!person.lastSeen() || this.isOnline(person))
-    }
 
     return (
       <tr className="person">
-        <td><img src={person.gravatar} className="gravatar" /></td>
-        <td className={isOffline.call(this, person) ? 'statusOffline' : 'statusOnline'}>{ person.name }</td>
+        <td><img alt={person.name} src={person.gravatar} className="gravatar" /></td>
+        <td className={PersonLine.isOffline.call(this, person) ? 'statusOffline' : 'statusOnline'}>{ person.name }</td>
         <td className="lastSeen">{ person.lastSeen() ? moment().to(person.lastSeen()) : 'never' }</td>
-        <td className="status">{ isOffline.call(this, person) ? <label className="offline">offline</label> : <label className="online">online</label> }</td>
+        <td className="status">
+          { PersonLine.isOffline.call(this, person)
+            ? <span className="offline">offline</span>
+            : <span className="online">online</span>
+          }
+        </td>
       </tr>
     )
-  },
-})
+  }
+}
+
+PersonLine.propTypes = {
+  person: React.PropTypes.instanceOf(Person).isRequired,
+}
 module.exports = PersonLine
