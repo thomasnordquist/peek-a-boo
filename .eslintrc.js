97a8d92ac888bf48fcc7a299987429b08b2f374d
Thomas Nordquist <thomas.nordquist@viison.com>
2017-03-31 12:19:45 +0200

    Add linter config

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "commonjs": true
+    },
+    "extends": "airbnb",
+    //"extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 8 // or 2017
+    },
+    "globals" : {
+        "require": false
+    },
+    "rules": {
+        "indent": [
+            "warn",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "warn",
+            "never"
+        ],
+        "no-unused-vars": [
+            "warn"
+        ],
+        "no-console": [
+            "warn"
+        ],
+        "no-restricted-syntax": [
+            "off"
+        ],
+        'array-bracket-spacing': ['error', 'never']
+    }
+};
