f0519acbb5e7eb437df2083aa3954d8d0c6f5065
Thomas Nordquist <thomas.nordquist@viison.com>
2017-03-31 12:37:29 +0200

    Add lastSeen to device model

--- Models/Device.js
+++ Models/Device.js
@@ -1,9 +1,10 @@
 module.exports = class Device {
-  constructor({ hostnames, mac, ip }) {
+  constructor({ hostnames, mac, lastSeen, ip }) {
     const hosts = hostnames || []
 
     this.hostname = hosts[0]
     this.mac = mac
+    this.lastSeen = lastSeen
     this.ip = ip
   }
 }
