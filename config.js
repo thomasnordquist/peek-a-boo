62e64f25f617c17f26ff34462ba6554a45d9f7e4
Thomas Nordquist <thomas.nordquist@viison.com>
2017-05-16 22:39:27 +0200

    Fix cors in production mode

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
   applicationPort: env.PORT ? env.PORT : '', /* the application will be served here (also possible to bind to host: 127.0.0.1:8080)*/
   bindToHost: null, /* enter an ip to bind the application port to a specific ip */
   offlineAfter: 60, /* 1 minute */
-  dev: env.DEV ? 'true' : 'false',
+  dev: env.DEV ? true : false,
 }
